2ceb12823d87df9405b51f1fede440b746582a0a
vladyslav.semenchuk <vladyslav.semenchuk@globallogic.com>
2019-05-05 14:54:15 +0300

    test cdn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express'),
     app = express();
 
 app.get('/', (req, res) => {
-  res.send('Fuck you!')
+  res.send('Fuck you bitch!')
 })
 
 app.listen(3000, () => console.log('Server running on port 3000'))
