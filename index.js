88717f59ffab9ef7e5310984283dc6b08cf7527d
vladyslav.semenchuk <vladyslav.semenchuk@globallogic.com>
2019-06-10 12:48:59 +0300

    try read database

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
-const express = require('express'),
-    app = express();
+const express = require('express');
+const AWS = require('aws-sdk');
+const awsConfig = {
+    "region": "us-east-1",
+    "endpoint": "http://dynamodb.us-east-1.amazonaws.com",
+    "accessKeyId": "AKIAIQY5N7STY3CWFFZA",
+    "secretAccesskey": "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"
+};
 
-app.get('/', (req, res) => {
-  res.send('Fuck you!')
+const app = express();
+
+AWS.config.update(awsConfig);
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const fetchData = function () {
+    const params = {
+        TableName: "VideoOnDemand"
+    };
+
+    docClient.get(params, function(err, data) {
+        if (err) {
+            console.log('Error: ' + JSON.stringify(err, null, 2));
+        } else {
+            console.log('Success: ' + JSON.stringify(data, null, 2));
+        }
+    });
+}
+
+app.get('/gallery', (req, res) => {
+    fetchData();
+    res.send('Fuck you!')
 })
 
 app.listen(3000, () => console.log('Server running on port 3000'))
