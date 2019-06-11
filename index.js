77691ae05bd44a06bbef9700a0ab58fa1f5f0ce6
vladyslav.semenchuk <vladyslav.semenchuk@globallogic.com>
2019-06-11 10:48:41 +0300

    ignore

--- index.js
+++ index.js
@@ -1,8 +1,47 @@
-const express = require('express'),
-    app = express();
+const express = require('express');
+const AWS = require('aws-sdk');
+const awsConfig = {
+    "region": "us-east-1",
+    "endpoint": "http://dynamodb.us-east-1.amazonaws.com",
+    "accessKeyId": "AKIAIEUKV2FDQG6UES3A",
+    "secretAccessKey": "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"
+};
+const cors = require('cors');
+const stringify = require('json-stringify-safe');
+const app = express();
+
+app.use(cors())
+
+AWS.config.update(awsConfig);
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+function fetchData(callback){
+    let response;
+
+    const scanningParams = {
+        TableName: 'VideoOnDemand',
+        Limit: 100
+    };
+
+    docClient.scan(scanningParams, function(err, data) {
+        if (err) {
+            console.log('Error: ' +  JSON.stringify(err, null, 2));
+        } else {
+            console.log('Success: ' +  JSON.stringify(data, null, 2))
+            callback(data);
+        }
+    });
+
+    return response;
+}
+
 
 app.get('/', (req, res) => {
-  res.send('Fuck you!')
+    fetchData(function(data) {
+        const { Items } = data;
+        res.send(Items);
+    });
 })
 
-app.listen(3000, () => console.log('Server running on port 3000'))
+app.listen(4000, () => console.log('Server running on port 4000'))
