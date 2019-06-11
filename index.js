57068ba740a545816d550a81ed19459e0b8afa17
vladyslav.semenchuk <vladyslav.semenchuk@globallogic.com>
2019-06-11 11:00:57 +0300

    try another port

--- index.js
+++ index.js
@@ -45,4 +45,4 @@ app.get('/', (req, res) => {
     res.send('Fuck');
 })
 
-app.listen(4000, () => console.log('Server running on port 4000'))
+app.listen(3000, () => console.log('Server running on port 3000'))
