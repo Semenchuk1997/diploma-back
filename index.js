b5dbfb0a1cf2188a8158731fe543e2acf953c6a1
vladyslav.semenchuk <vladyslav.semenchuk@globallogic.com>
2019-06-11 10:21:20 +0300

    implemented get endpoint to read data from database

--- index.js
+++ index.js
@@ -3,33 +3,45 @@ const AWS = require('aws-sdk');
 const awsConfig = {
     "region": "us-east-1",
     "endpoint": "http://dynamodb.us-east-1.amazonaws.com",
-    "accessKeyId": "AKIAIQY5N7STY3CWFFZA",
-    "secretAccesskey": "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"
+    "accessKeyId": "AKIAIEUKV2FDQG6UES3A",
+    "secretAccessKey": "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"
 };
-
+const cors = require('cors');
+const stringify = require('json-stringify-safe');
 const app = express();
+
+app.use(cors())
 
 AWS.config.update(awsConfig);
 
 const docClient = new AWS.DynamoDB.DocumentClient();
 
-const fetchData = function () {
-    const params = {
-        TableName: "VideoOnDemand"
+function fetchData(callback){
+    let response;
+
+    const scanningParams = {
+        TableName: 'VideoOnDemand',
+        Limit: 100
     };
 
-    docClient.get(params, function(err, data) {
+    docClient.scan(scanningParams, function(err, data) {
         if (err) {
-            console.log('Error: ' + JSON.stringify(err, null, 2));
+            console.log('Error: ' +  JSON.stringify(err, null, 2));
         } else {
-            console.log('Success: ' + JSON.stringify(data, null, 2));
+            console.log('Success: ' +  JSON.stringify(data, null, 2))
+            callback(data);
         }
     });
+
+    return response;
 }
 
+
 app.get('/', (req, res) => {
-    const data = fetchData();
-    res.send(data);
+    fetchData(function(data) {
+        const { Items } = data;
+        res.send(Items);
+    });
 })
 
-app.listen(3000, () => console.log('Server running on port 3000'))
+app.listen(4000, () => console.log('Server running on port 4000'))
