44687c5c405a58800762c356211ac769d8f39604
vladyslav.semenchuk <vladyslav.semenchuk@globallogic.com>
2019-06-11 22:20:00 +0300

    fix prev bug

--- index.js
+++ index.js
@@ -38,7 +38,8 @@ function fetchData(callback){
 app.get('/', (req, res) => {
     fetchData(function(data) {
         const { Items } = data;
-        const responseData = { thumbnailUrl: Items.thumbnailUrl[0], mp4Url: Items.mp4Urls[0] }
+        const responseData = Items.map(item => ({ thumbnailUrl: item.thumbnailUrl[0], mp4Url: item.mp4Urls[0] }));
+
         res.send(responseData);
     });
 })
